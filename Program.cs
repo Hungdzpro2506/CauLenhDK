461cc2f99024cab0fba30df1d58b3ad8f00d0f5d
Hungdzpro2506 <134621730+Hungdzpro2506@users.noreply.github.com>
2024-11-18 21:19:10 +0700

    Add Program.cs file

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            bool isNamNhuan = false;
+            Console.WriteLine("nhap vao nam muon ktra :");
+            year = Convert.ToInt32(Console.ReadLine());
+            bool Chiahet4 = year % 4 == 0;
+            if (Chiahet4)
+            {
+                bool Chiahet100 = year % 100 == 0;
+                if (Chiahet100) 
+                {
+                   bool Chiahet400= year %400== 0;
+                    if(Chiahet400)
+                    {
+                        isNamNhuan= true;
+                    }
+                    
+
+                }
+                else { isNamNhuan = true; }
+            }
+            if (isNamNhuan)
+            {
+                Console.WriteLine("{0} la nam nhuan", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} khong phai nam nhuan", year);
+            }
+
+        }
+    }
+}
